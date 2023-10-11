0add5098c8397b901b2b0e7a282fde60e73092f7
Hector Gomez <42254931+hemagome@users.noreply.github.com>
2023-10-11 16:20:22 -0500

    Remoción .env

--- src/app/api/eps/route.tsx
+++ src/app/api/eps/route.tsx
@@ -3,6 +3,5 @@ import { eps } from '@/lib/schema'
 import { NextResponse } from 'next/server'
 
 export const GET = async () => {
-	// const data = await db.select().from(eps).all()
 	return NextResponse.json(await db.select().from(eps).all())
 }
