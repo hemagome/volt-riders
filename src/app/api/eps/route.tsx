ad7828fa2f6ffa7e97fbaeb1a4611f56fd2da679
Hector Gomez <42254931+hemagome@users.noreply.github.com>
2023-10-10 22:52:10 -0500

    Implementación Drizzle

---
+++ src/app/api/eps/route.tsx
@@ -0,0 +1,8 @@
+import { db } from '@/lib/turso'
+import { eps } from '@/lib/schema'
+import { NextResponse } from 'next/server'
+
+export const GET = async () => {
+	const data = await db.select().from(eps).all()
+	return NextResponse.json({ data })
+}
---
+++ src/lib/schema.ts
@@ -0,0 +1,9 @@
+import { sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { InferSelectModel } from 'drizzle-orm'
+
+export const eps = sqliteTable('eps', {
+	id: text('id').primaryKey(),
+	name: text('name').notNull(),
+})
+
+export type Eps = InferSelectModel<typeof eps>
