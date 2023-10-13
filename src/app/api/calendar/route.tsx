38e68dfca82cf163c1e34365d766f29d15aa1bea
Hector Gomez <42254931+hemagome@users.noreply.github.com>
2023-10-13 17:19:52 -0500

    Modificación para corregir error

--- src/app/api/calendar/route.tsx
+++ src/app/api/calendar/route.tsx
@@ -1,8 +1,8 @@
 import { db } from "@/lib/turso";
-import { event } from "@/lib/schema";
+import { eps, event } from "@/lib/schema";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
-  const data = await db.select().from(event).all();
+  const data = await db.select().from(eps).all();
   return NextResponse.json(data);
 };
