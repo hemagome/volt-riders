c6e59eda31a53e9cbece7a8fdf73df8a85b1e9fa
hemagome28 <hemagome-28@hotmail.com>
2023-10-14 22:40:36 -0500

    Actualización dependencias

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return <SignIn
+    appearance={{
+      elements: {
+        formButtonPrimary:
+          "bg-[#29235C] hover:bg-[#13152E] text-sm normal-case",
+      },
+    }}
+     />;
 }
