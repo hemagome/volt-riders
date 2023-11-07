d9b83a38bdfc654bade2630c715394b713c99016
Hector Gomez <42254931+hemagome@users.noreply.github.com>
2023-11-06 20:54:50 -0500

    Optimización carrousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,52 @@
+"use client";
 import { useState } from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, DotIcon } from "lucide-react";
 
 export default function Home() {
-  const slides = [{ url: "/Feas.jpg" }, { url: "/VoltRiders.webp" }];
+  const slides = [
+    { url: "/WeRacingRuedas.jpg" },
+    { url: "/WeRacingRuedas2.jpg" },
+    { url: "/Rodada1.jpg" },
+    { url: "/Rodada2.jpg" },
+    { url: "/Rodada3.jpg" },
+  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const prevSlide = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+  const nextSlide = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+  const goToSlide = (slideIndex: number) => {
+    setCurrentIndex(slideIndex);
+  };
 
   return (
-    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative">
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
       <div
-        style={{ backgroundImage: `url(${slides[0].url})` }}
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
-      >
-        <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-          <ChevronLeft size={30} />
-        </div>
-        <div className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-          <ChevronRight size={30} />
-        </div>
+      ></div>
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <ChevronLeft onClick={prevSlide} size={30} />
+      </div>
+      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <ChevronRight onClick={nextSlide} size={30} />
+      </div>
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((_slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className="text-2xl cursor-pointer"
+          >
+            <DotIcon />
+          </div>
+        ))}
       </div>
     </div>
   );
